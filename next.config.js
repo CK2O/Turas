f9ea597d42b75021222d950ea23ec167ae8064ec
CK2O <cormac.kerins@gmail.com>
2025-03-19 21:04:06 +0100

    Optimize Next.js config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -3,9 +3,12 @@ const nextConfig = {
   output: 'standalone',
   images: {
     domains: ['images.unsplash.com'], // Add any external image domains you're using
+    unoptimized: process.env.NODE_ENV === 'development'
   },
   poweredByHeader: false,
   reactStrictMode: true,
+  swcMinify: true,
+  compress: true
 };
 
 module.exports = nextConfig; 
